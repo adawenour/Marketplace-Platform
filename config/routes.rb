3483137d861602885d6802cb501b10339832fa90
Adawe Nour <nouradawe@gmail.com>
2014-04-30 16:41:34 -0400

    Creating controllers,routes and Listing resource

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Rails.application.routes.draw do
+  resources :listings
+
+  get 'pages/about'
+
+  get 'pages/contact'
+
+  root 'listings#index'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
