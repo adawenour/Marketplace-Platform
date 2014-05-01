e35e983f71a506379a256ed0cd01a5520218ab58
Adawe Nour <nouradawe@gmail.com>
2014-05-01 18:24:23 -0400

    Updated front-end design

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140430202020) do
+ActiveRecord::Schema.define(version: 20140430203856) do
 
   create_table "listings", force: true do |t|
     t.string   "name"
