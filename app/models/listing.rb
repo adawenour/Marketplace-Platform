bc198fc6348a533529eba9b43937df58ae3547a1
Adawe Nour <nouradawe@gmail.com>
2014-05-05 21:30:10 -0400

    updated listings controller

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -1,5 +1,5 @@
 class Listing < ActiveRecord::Base
 	has_attached_file :image, :styles => { :medium => "200x", :thumb => "100x100>" }, :default_url => "nophoto.jpg"
-	 do_not_validate_attachment_file_type :image
+	validates_attachment :image, :content_type => { :content_type => %w(image/jpeg image/jpg image/png) } 
 end
 
